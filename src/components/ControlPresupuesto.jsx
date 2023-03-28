55921a93e2d7c663f8d1dd8832fb9c5f4f95683f
Matías <matiasagbenitez@gmail.com>
2023-03-28 17:31:56 -0300

    Gráfico circular para el control de gastos

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export const ControlPresupuesto = ({ presupuesto, gastos }) => {
   const [disponible, setDisponible] = useState(0);
   const [gastado, setGastado] = useState(0);
+  const [porcentaje, setPorcentaje] = useState(0);
 
   useEffect(() => {
-    const totalGastado = gastos.reduce((total, gasto) => total + gasto.cantidad, 0);
+    const totalGastado = gastos.reduce(
+      (total, gasto) => total + gasto.cantidad,
+      0
+    );
     setGastado(totalGastado);
     setDisponible(presupuesto - totalGastado);
+    setTimeout(() => {
+      setPorcentaje(((totalGastado / presupuesto) * 100).toFixed(2));
+    }, 1000);
   }, [gastos]);
 
   const formatearCantidad = (cantidad) => {
@@ -19,7 +28,17 @@ export const ControlPresupuesto = ({ presupuesto, gastos }) => {
 
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-      <div>Grafica aqui</div>
+      <div>
+        <CircularProgressbar 
+          value={porcentaje}
+          text={`${porcentaje}% gastado`}
+          styles={buildStyles({
+            pathColor: "#3B82F6",
+            trailColor: "#D1D5DB",
+            textColor: "#3B82F6",
+          })}
+        />
+      </div>
 
       <div className="contenido-presupuesto">
         <p>
